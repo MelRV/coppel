cd2909f5996bb40dcbc2cdda0cbcba4f85f5d858
Katya Laso <kat_jenny@hotmail.com>
2024-02-06 11:22:18 -0600

    log in fail

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   
-    chromeWebSecurity: false,
-    chromeHeadless: false,
-    pageLoadTimeout: 60000,
+    /* chromeWebSecurity: false,
+    chromeHeadless: false, */
+    pageLoadTimeout: 90000,
   
     e2e: {
     setupNodeEvents(on, config) {
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,13 +5,28 @@ describe('template spec', () => {
   before('Open application', () => {
     cy.clearCookies()
     cy.clearLocalStorage();
+      // Disable service workers before each test
+  if (window.navigator && navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations()
+      .then((registrations) => {
+        registrations.forEach((registration) => {
+          registration.unregister();
+        });
+      });
+  }
         }) 
   it('passes', () => {
     cy.visit("https://www.coppel.com/LogonForm?myAcctMain=1&catalogId=10051&langId=-5&storeId=10151", { headers: { "Accept-Encoding": "gzip, deflate" } });
     cy.get('input[type="text"]').should('be.visible').type(dataLogin.username)
-    cy.get('input[type="password"]').should('be.visible').type(dataLogin.password)
-    cy.get('div[data-login="inicio_sesion"]',{timeout:10000}).click().wait(10000);
+    cy.get('input[type="password"]').should('be.visible').type(dataLogin.password).wait(10000)
+    cy.clearCookies().wait(10000)
+    cy.get('.button_align', { timeout: 20000 }).should('exist').should('be.visible').click({force: true}).wait(10000)
 
+
+
+//#btn-login
+//WC_AccountDisplay_links_2
+//div[data-login="inicio_sesion"]
 
   })
 })
